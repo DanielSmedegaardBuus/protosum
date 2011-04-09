3789c6fef121c40e9460285634f913e92dfba466
Daniel Smedegaard Buus <daniel@danielsmedegaardbuus.dk>
2011-04-09 12:20:54 +0200

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Protosum::Application.config.secret_token = '4ece21950f94b8184cd1806da7de37fb5252c2faf89975c9414e4049ada17fc96fbf8798841405914fe3ccf51c524e2762cae4a1c13dec4362176c6379c520a9'
